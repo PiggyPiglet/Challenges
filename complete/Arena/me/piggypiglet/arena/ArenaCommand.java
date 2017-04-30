30a4b75d1e0f793ebd91adcaa1b2845fee82d5d8
PiggyPiglet <PiggyPiglet@WickedCubeMC.com>
2017-04-30 15:49:30 +0800

    Arena
    Arena plugin

---
+++ complete/Arena/me/piggypiglet/arena/ArenaCommand.java
@@ -0,0 +1,33 @@
+package me.piggypiglet.arena;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ArenaCommand implements CommandExecutor {
+    Arena plugin;
+
+    public ArenaCommand(Arena instance) {
+        plugin = instance;
+    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (cmd.getName().equalsIgnoreCase("setarena")) {
+                plugin.getConfig().set("x", player.getLocation().getBlockX());
+                plugin.getConfig().set("y", player.getLocation().getBlockY());
+                plugin.getConfig().set("z", player.getLocation().getBlockZ());
+                plugin.saveConfig();
+                player.sendRawMessage("§7Arena spawn set to §c" + player.getLocation().getBlockX() + " §c" + player.getLocation().getBlockY() + " §c" + player.getLocation().getBlockZ());
+            }
+            if (cmd.getName().equalsIgnoreCase("arena")) {
+                player.teleport(new Location(Bukkit.getWorld("world"), plugin.getConfig().getInt("x"), plugin.getConfig().getInt("y"), plugin.getConfig().getInt("z") ));
+            }
+        }
+        return true;
+    }
+}
